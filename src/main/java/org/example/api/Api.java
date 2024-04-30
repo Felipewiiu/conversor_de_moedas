283c48ff8773d7e0382c19d99ca0f0742b1362c4
felipewiiu3 <felipewiiu3@gmail.com>
2024-04-30 13:52:50 -0300

    first commit

---
+++ src/main/java/org/example/api/Api.java
@@ -0,0 +1,22 @@
+package org.example.api;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Api {
+    // Setting URL
+    String url_str = "https://v6.exchangerate-api.com/v6/6c4a71f9adc8560015a27e23/latest/USD";
+
+    // Making Request
+    URL url = new URL(url_str);
+    HttpURLConnection request = (HttpURLConnection) url.openConnection();
+request.connect();
+
+    // Convert to JSON
+    JsonParser jp = new JsonParser();
+    JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+    JsonObject jsonobj = root.getAsJsonObject();
+
+    // Accessing object
+    String req_result = jsonobj.get("result").getAsString();
+}
